fixed KL / NLL size issue; analytic KL is cleaner also removed mnist_categorical model

interactive_renderer.py
@@ -41,7 +41,7 @@ vae.model.load_weights(weights_file)
 class_colors = get_class_colors(10)
 
 # Get keras function of recognition model.
-q = K.function([vae.q_model.input], [vae.latent.mean, vae.latent.log_var])
+q = K.function([vae.inpt], [vae.latents[0].mean, vae.latents[0].log_var])
 
 # Apply recognition model to test set.
 pred_mean, pred_log_var = q([x_test])
@@ -114,7 +114,7 @@ main_surface.blit(pygame.transform.scale(ent_surface, (400, 400)), (0, 0, 400, 4
 # CONSTRUCT RENDERER #
 ######################
 
-render = K.function([vae.latent_sample], [vae.reconstruction])
+render = K.function([vae.latents[0].flat_samples], [vae.reconstruction])
 renderer_input = np.zeros((1, 2))
 
 ####################

mnist_entropy.py
@@ -22,7 +22,7 @@ img_pixels = img_rows * img_cols
 model_type = argv[1]
 latent_dim = 2
 pixel_std = .05
-k_samples = 32
+k_samples = 16
 
 vae = gaussian_mnist(model_type, latent_dim=latent_dim, pixel_std=pixel_std, k=k_samples)
 vae.model.compile(loss=None, optimizer='rmsprop')
@@ -38,7 +38,7 @@ if os.path.exists(weights_file):
 else:
     vae.model.fit(x_train,
                   shuffle=True,
-                  epochs=15,
+                  epochs=50,
                   batch_size=100,
                   validation_data=(x_test, None))
     # Save trained model to a file
@@ -53,7 +53,7 @@ else:
 class_colors = get_class_colors(10)
 
 # Get keras function of recognition model.
-q = K.function([vae.q_model.input], [vae.latent.mean, vae.latent.log_var])
+q = K.function([vae.inpt], [vae.latents[0].mean, vae.latents[0].log_var])
 
 # Apply recognition model to test set.
 pred_mean, pred_log_var = q([x_test])
@@ -62,7 +62,7 @@ pred_std = np.sqrt(pred_var)
 
 # Evaluate distribution over a set of points in the latent space -- get a categorical distribution
 # over classes at each point
-categorical, latent_extent = class_categorical(pred_mean, pred_std, y_test, 10, res=100)
+categorical, latent_extent = class_categorical(pred_mean, pred_std, y_test, res=100)
 
 # Find what class is most (plurality) represented at each point
 max_class = categorical.argmax(axis=2)
@@ -86,25 +86,4 @@ plt.title('Entropy over classes')
 plt.subplot(133)
 plt.imshow(entropy_image, extent=latent_extent)
 plt.title('Adjusted class in latent space')
-plt.show()
-
-#################
-# VISUALIZATION #
-#################
-
-render_range = 5
-grid = 15
-vae.set_samples(1)
-render = K.function([vae.latent_sample], [vae.reconstruction])
-full_image = np.zeros((img_rows * grid, img_cols * grid))
-xs = np.linspace(-render_range, render_range, grid)
-xx, yy = np.meshgrid(xs, xs)
-inputs = np.vstack([yy.ravel(), xx.ravel()]).T
-outputs = render([inputs])[0]
-for ii in range(grid**2):
-    i, j = divmod(ii, grid)
-    full_image[j * img_cols:(j + 1) * img_cols, i * img_rows:(i + 1) * img_rows] = \
-        np.reshape(outputs[ii], (img_rows, img_cols))
-plt.imshow(full_image, cmap='Greys_r',
-           extent=(-render_range, render_range, -render_range, render_range))
 plt.show()

models.py
@@ -1,7 +1,9 @@
 from vae import VAE, IWAE, IsoGaussianPrior, DiagonalGaussianLatent, GaussianLikelihood, \
     CategoricalLatent, DiscreteUniformPrior
-from keras.models import Sequential, Model
+from keras.models import Sequential
 from keras.layers import Input, Dense, Conv2D, Conv2DTranspose, Reshape, Flatten
+from keras.engine.topology import Layer
+import numpy as np
 import keras.backend as K
 
 
@@ -12,6 +14,20 @@ omniglot_rows, omniglot_cols, omniglot_channels = 64, 64, 1
 omniglot_pixels = omniglot_rows * omniglot_cols
 
 
+class FoldSamplesIntoBatch(Layer):
+    def call(self, x):
+        input_shape = K.shape(x)
+        new_shape = K.concatenate([[input_shape[0] * input_shape[1]], input_shape[2:]], axis=0)
+        return K.reshape(x, new_shape)
+
+    def compute_output_shape(self, input_shape):
+        print("FOLD SHAPE", input_shape)
+        if input_shape[0] is None:
+            return (None,) + tuple(input_shape[2:])
+        else:
+            return (input_shape[0] * input_shape[1],) + tuple(input_shape[2:])
+
+
 def gaussian_omniglot(cls='vae', latent_dim=2, pixel_std=.05, k=1):
     # number of convolutional filters to use
     filters = 64
@@ -19,80 +35,83 @@ def gaussian_omniglot(cls='vae', latent_dim=2, pixel_std=.05, k=1):
     num_conv = 3
     intermediate_dim = 128
 
-    # RECOGNITION MODEL
+    # GLOBAL 'SAMPLES' VARIABLE
+    k_samples = 16  # K.variable(k, dtype='int32', name='k_samples')
+
+    # Get shapes with correctly-ordered dimensions
     if K.image_data_format() == 'channels_first':
         original_img_size = (omniglot_channels, omniglot_rows, omniglot_cols)
+        output_shape = (filters, omniglot_rows // 2, omniglot_cols // 2)
+        output_shape_samples = (k_samples, filters, omniglot_rows // 2, omniglot_cols // 2)
     else:
         original_img_size = (omniglot_rows, omniglot_cols, omniglot_channels)
+        output_shape = (omniglot_rows // 2, omniglot_cols // 2, filters)
+        output_shape_samples = (k_samples, omniglot_rows // 2, omniglot_cols // 2, filters)
 
-    x = Input(shape=original_img_size)
+    # RECOGNITION MODEL
+    inpt = Input(shape=original_img_size)
     conv_1 = Conv2D(omniglot_channels,
                     kernel_size=(2, 2),
-                    padding='same', activation='relu')(x)
+                    padding='same',
+                    activation='relu')(inpt)
     conv_2 = Conv2D(filters,
                     kernel_size=(2, 2),
-                    padding='same', activation='relu',
+                    padding='same',
+                    activation='relu',
                     strides=(2, 2))(conv_1)
     conv_3 = Conv2D(filters,
                     kernel_size=num_conv,
-                    padding='same', activation='relu',
+                    padding='same',
+                    activation='relu',
                     strides=1)(conv_2)
     conv_4 = Conv2D(filters,
                     kernel_size=num_conv,
-                    padding='same', activation='relu',
+                    padding='same',
+                    activation='relu',
                     strides=1)(conv_3)
     flat = Flatten()(conv_4)
-    intermediate = Dense(intermediate_dim, activation='relu')(flat)
-    q_model = Model(x, intermediate)
+    rec_fc1 = Dense(intermediate_dim, activation='relu')(flat)
 
-    # LATENT
-    latent = DiagonalGaussianLatent(dim=latent_dim)
-
-    # PRIOR
-    prior = IsoGaussianPrior(dim=latent_dim)
+    # LATENT -- PRIOR
+    latent = DiagonalGaussianLatent(dim=latent_dim, k_samples=k_samples, prior=IsoGaussianPrior)
+    latent_samples = latent(rec_fc1)
 
     # GENERATIVE MODEL
-    p_model = Sequential()
-    p_model.add(Dense(intermediate_dim, activation='relu', input_dim=latent_dim))
-    p_model.add(Dense(filters * omniglot_rows // 2 * omniglot_cols // 2, activation='relu'))
+    gen_fc1 = Dense(intermediate_dim, activation='relu')(latent_samples)
+    gen_pre_deconv_flat = Dense(np.prod(output_shape), activation='relu')(gen_fc1)
+    gen_pre_deconv = Reshape(output_shape_samples)(gen_pre_deconv_flat)
+    fold_gen_pre_deconv = FoldSamplesIntoBatch()(gen_pre_deconv)
 
-    if K.image_data_format() == 'channels_first':
-        output_shape = (filters, omniglot_rows // 2, omniglot_cols // 2)
-    else:
-        output_shape = (omniglot_rows // 2, omniglot_cols // 2, filters)
-
-    p_model.add(Reshape(output_shape))
-    p_model.add(Conv2DTranspose(filters,
-                                kernel_size=num_conv,
-                                padding='same',
-                                strides=1,
-                                activation='relu'))
-    p_model.add(Conv2DTranspose(filters,
-                                kernel_size=num_conv,
-                                padding='same',
-                                strides=1,
-                                activation='relu'))
-    p_model.add(Conv2DTranspose(filters,
-                                kernel_size=(3, 3),
-                                strides=(2, 2),
-                                padding='valid',
-                                activation='relu'))
-    p_model.add(Conv2D(omniglot_channels,
-                       kernel_size=2,
-                       padding='valid',
-                       activation='sigmoid'))
+    deconv_1 = Conv2DTranspose(filters,
+                               kernel_size=num_conv,
+                               padding='same',
+                               strides=1,
+                               activation='relu')(fold_gen_pre_deconv)
+    deconv_2 = Conv2DTranspose(filters,
+                               kernel_size=num_conv,
+                               padding='same',
+                               strides=1,
+                               activation='relu')(deconv_1)
+    deconv_3_upsample = Conv2DTranspose(filters,
+                                        kernel_size=(3, 3),
+                                        strides=(2, 2),
+                                        padding='valid',
+                                        activation='relu')(deconv_2)
+    reconstruction = Conv2D(omniglot_channels,
+                            kernel_size=2,
+                            padding='valid',
+                            activation='sigmoid')(deconv_3_upsample)
 
     # LIKELIHOOD
     likelihood = GaussianLikelihood(pixel_std)
 
     # Combine the above parts into a single model
     kwargs = {
-        'q_model': q_model,
-        'latent': latent,
-        'prior': prior,
-        'p_model': p_model,
+        'inpt': inpt,
+        'latents': [latent],
+        'reconstruction': reconstruction,
         'likelihood': likelihood,
-        'k': k
+        'k_samples': k_samples
     }
     if cls == 'vae':
         return VAE(**kwargs)
@@ -101,21 +120,22 @@ def gaussian_omniglot(cls='vae', latent_dim=2, pixel_std=.05, k=1):
 
 
 def gaussian_mnist(cls='vae', latent_dim=2, pixel_std=.05, k=1):
-    inpt = Input(shape=(mnist_pixels,))
+    # GLOBAL 'SAMPLES' VARIABLE
+    k_samples = K.variable(k, dtype='int32', name='k_samples')
 
     # RECOGNITION MODEL
-    k_samples = K.variable(k, dtype='int32', name='k_samples')
+    inpt = Input(shape=(mnist_pixels,))
     q_hidden_1 = Dense(64, activation='relu')(inpt)
-    # q_hidden_2 = Dense(64, activation='relu')(q_hidden_1)
+    q_hidden_2 = Dense(64, activation='relu')(q_hidden_1)
 
     # LATENT -- PRIOR
     latent = DiagonalGaussianLatent(dim=latent_dim, k_samples=k_samples, prior=IsoGaussianPrior)
-    latent_samples = latent(q_hidden_1)
+    latent_samples = latent(q_hidden_2)
 
     # GENERATIVE MODEL
     gen_hidden_1 = Dense(64, activation='relu')(latent_samples)
-    # gen_hidden_2 = Dense(64, activation='relu')(gen_hidden_1)
-    reconstruction = Dense(mnist_pixels, activation='sigmoid')(gen_hidden_1)
+    gen_hidden_2 = Dense(64, activation='relu')(gen_hidden_1)
+    reconstruction = Dense(mnist_pixels, activation='sigmoid')(gen_hidden_2)
 
     # LIKELIHOOD
     likelihood = GaussianLikelihood(pixel_std)
@@ -145,42 +165,6 @@ def mnist_hvae(cls='vae', latent_dim=2, layers=3, pixel_std=.05, k=1):
 
     # PRIOR
     prior = IsoGaussianPrior(dim=latent_dim)
-
-    # GENERATIVE MODEL
-    p_model = Sequential()
-    p_model.add(Dense(64, activation='relu', input_dim=latent_dim))
-    p_model.add(Dense(64, activation='relu'))
-    p_model.add(Dense(mnist_pixels, activation='sigmoid'))
-
-    # LIKELIHOOD
-    likelihood = GaussianLikelihood(pixel_std)
-
-    # Combine the above parts into a single model
-    kwargs = {
-        'q_model': q_model,
-        'latent': latent,
-        'prior': prior,
-        'p_model': p_model,
-        'likelihood': likelihood,
-        'k': k
-    }
-    if cls == 'vae':
-        return VAE(**kwargs)
-    elif cls == 'iwae':
-        return IWAE(**kwargs)
-
-
-def categorical_mnist(cls='vae', latent_dim=10, pixel_std=.05, k=1):
-    # RECOGNITION MODEL
-    q_model = Sequential()
-    q_model.add(Dense(64, activation='relu', input_dim=mnist_pixels))
-    q_model.add(Dense(64, activation='relu'))
-
-    # LATENT
-    latent = CategoricalLatent(dim=latent_dim)
-
-    # PRIOR
-    prior = DiscreteUniformPrior(dim=latent_dim)
 
     # GENERATIVE MODEL
     p_model = Sequential()

util.py
@@ -23,7 +23,7 @@ def class_categorical(means, stds, classes, res=100, eps=1e-10):
     pts = np.concatenate([np.expand_dims(xx, 2), np.expand_dims(yy, 2)], 2)
 
     # Create a categorical distribution over classes at each point in the grid/image
-    categorical = np.full(xx.shape + (classes.unique().size,), eps)
+    categorical = np.full(xx.shape + (np.unique(classes).size,), eps)
 
     def multivariate_gaussian(pos, mu, sigma):
         """Return the multivariate Gaussian distribution on array pos.

vae/latents.py
@@ -1,7 +1,6 @@
 from keras.engine.topology import Layer
 from tensorflow.contrib.distributions import OneHotCategorical
 from .priors import IsoGaussianPrior, DiscreteUniformPrior
-import numpy as np
 import keras.backend as K
 
 
@@ -12,6 +11,7 @@ class FoldSamplesIntoBatch(Layer):
         return K.reshape(x, new_shape)
 
     def compute_output_shape(self, input_shape):
+        print("FOLD SHAPE", input_shape)
         if input_shape[0] is None:
             return (None,) + tuple(input_shape[2:])
         else:
@@ -23,18 +23,21 @@ class Latent(Layer):
         super(Latent, self).__init__(**kwargs)
 
         # Convert k_samples to keras variable (unless it already is one)
-        try:
-            self.k_samples = K.variable(int(k_samples), name="k_samples", dtype=np.int32)
-        except TypeError:
-            self.k_samples = k_samples
+        # try:
+        #     self.k_samples = K.variable(int(k_samples), name="k_samples", dtype=np.int32)
+        # except TypeError:
+        self.k_samples = k_samples
 
         # Record instance variables
         self.d = dim
         self.prior = prior(dim)
 
     def sample_kl(self):
-        # Use monte carlo kl estimate
-        return self.log_prob(self.samples) - self.prior.log_prob(self.samples)
+        try:
+            return K.tile(K.expand_dims(self.analytic_kl()), (1, self.k_samples))
+        except ValueError:
+            # Use monte carlo kl estimate
+            return self.log_prob(self.samples) - self.prior.log_prob(self.samples)
 
     def compute_output_shape(self, input_shape):
         return tuple(input_shape[:-1]) + (self.k_samples, self.d)
@@ -46,6 +49,9 @@ class Latent(Layer):
     def log_prob(self, x):
         # Must be implemented by subclass
         raise NotImplementedError()
+
+    def analytic_kl(self):
+        raise ValueError()
 
 
 class DiagonalGaussianLatent(Latent):
@@ -71,11 +77,13 @@ class DiagonalGaussianLatent(Latent):
         # Create (reparameterized) sample from the latent distribution
         sample_shape = (K.shape(self.mean)[0], self.k_samples, self.d)
         eps = K.random_normal(shape=sample_shape, mean=0., stddev=1.0)
-        self.samples = rep_mean + eps * rep_std
 
-        # Samples of shape (batch, samples, ...) are saved in self.samples. For input into the
-        # generative model, 'fold' the samples into the batch dimension
-        return FoldSamplesIntoBatch()(self.samples)
+        # Shape of self.samples is (batch, samples, dim)
+        self.samples = rep_mean + eps * rep_std
+        # Shape of self.flat_samples is (batch * samples, dim)
+        self.flat_samples = FoldSamplesIntoBatch()(self.samples)
+
+        return self.flat_samples
 
     def log_prob(self, x):
         x_samples = K.shape(x)[1]
@@ -84,7 +92,7 @@ class DiagonalGaussianLatent(Latent):
         x_diff = x - K.repeat(self.mean, x_samples)
         return -(K.sum((x_diff / variance) * x_diff, axis=-1) + log_det) / 2
 
-    def sample_kl(self):
+    def analytic_kl(self):
         if isinstance(self.prior, IsoGaussianPrior):
             # In general for two multi-variate normals
             #   kl(p1||p2)=[log(det(C2)/det(C1)) - dim + Tr(C2^-1*C1) + (m2-m1).T*C2^-1*(m2-m1)]/2
@@ -96,7 +104,7 @@ class DiagonalGaussianLatent(Latent):
             mean_sq_norm = K.sum(self.mean**2, axis=-1)
             return (-log_det_p1 - self.d + trace_c1 + mean_sq_norm) / 2
         else:
-            return super(DiagonalGaussianLatent, self).sample_kl()
+            raise ValueError("Prior must be IsoGaussianPrior")
 
 
 class CategoricalLatent(Latent):
@@ -115,10 +123,11 @@ class CategoricalLatent(Latent):
         # samples, dim) with float type.
         ohc_sample = OneHotCategorical(logits=self.logits).sample(self.k_samples)
         self.samples = K.cast(K.permute_dimensions(ohc_sample, (1, 0, 2)), 'float32')
+        self.flat_samples = FoldSamplesIntoBatch()(self.samples)
 
         # Samples of shape (batch, samples, ...) are saved in self.samples. For input into the
         # generative model, 'fold' the samples into the batch dimension
-        return FoldSamplesIntoBatch()(self.samples)
+        return self.flat_samples
 
     def log_prob(self, x):
         x_samples = K.shape(x)[1]
@@ -129,7 +138,7 @@ class CategoricalLatent(Latent):
             # p is unnormalized multinomial probability
             p = K.exp(self.logits)
             # partition is normalization constant (partition function)
-            partition = K.sum(p, axis=-1)
+            partition = K.sum(p, axis=-1, keepdims=True)
             return K.sum(p * self.logits, axis=-1) / partition - K.log(partition) + K.log(self.d)
         else:
-            return super(DiagonalGaussianLatent, self).sample_kl()
+            raise ValueError("Prior must be DiscreteUniformPrior")